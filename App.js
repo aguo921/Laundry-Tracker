cb7f7def91cdfc8a6b6c9e9ad368ad520a8a8ac9
aguo921 <aguo921@aucklanduni.ac.nz>
2023-05-06 15:14:44 +1200

    Add navigation

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React, { useState } from 'react';
-import CurrentLaundry from './Components/CurrentLaundry';
-import Settings from './Components/Settings';
+import LaundryScreen from './Components/LaundryScreen';
+import SettingsScreen from './Components/SettingsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
   const defaultData = [{
@@ -10,18 +12,45 @@ export default function App() {
     name: 'Shirt',
     description: 'Red',
     maxWears: 5,
-    wears: 1
+    wears: 1,
+    notes: ""
   }
   ]
   const [data, setData] = useState(defaultData);
   const [multiplier, setMultiplier] = useState(1);
 
+  const Tab = createBottomTabNavigator();
+
   return (
-    <View style={styles.container}>
-      <CurrentLaundry data={data} setData={setData} multiplier={multiplier}/>
-      <Settings multiplier={multiplier} setMultiplier={setMultiplier}/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Laundry"
+        screenOptions={({route}) => ({
+          tabBarIcon: ({color, size}) => {
+            let iconName;
+
+            if (route.name=="Laundry") {
+              iconName = 'home'
+            } else {
+              iconName = 'setting'
+            }
+
+            return <AntDesign name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'gray',
+          tabBarActiveBackgroundColor: 'lightgray'
+        })}
+      >
+        <Tab.Screen name="Laundry">
+          {(props) => <LaundryScreen {...props} data={data} setData={setData} multiplier={multiplier}/>}
+        </Tab.Screen>
+
+        <Tab.Screen name="Settings">
+          {(props) => <SettingsScreen {...props} multiplier={multiplier} setMultiplier={setMultiplier}/>}
+        </Tab.Screen>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
